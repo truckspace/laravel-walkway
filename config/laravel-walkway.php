3159d6ed3bc84923e2929cd743cca99e776b42c4
Ben Sherred <ben@sherred.co.uk>
2020-12-31 12:53:51 +0000

    Add config documentation

--- config/laravel-walkway.php
+++ config/laravel-walkway.php
@@ -2,6 +2,16 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Base URL
+    |--------------------------------------------------------------------------
+    |
+    | The base url used for making requests. You usually won't need to change
+    | this, unless you have access to a staging environment.
+    |
+    */
+
     'base_url' => env('TRUCKSPACE_BASE_URL', 'https://id.truckspace.group'),
 
     /*
