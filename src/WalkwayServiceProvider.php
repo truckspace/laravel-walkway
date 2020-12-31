1427a36df297e37aa3bcee11d8d532c9b210dd9b
Ben Sherred <ben@sherred.co.uk>
2020-12-31 13:24:35 +0000

    Refactor registering migrations

--- src/WalkwayServiceProvider.php
+++ src/WalkwayServiceProvider.php
@@ -40,37 +40,8 @@ class WalkwayServiceProvider extends ServiceProvider
     protected function configurePublishing(): void
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/laravel-walkway.php' => config_path('laravel-walkway.php'),
-            ], 'walkway-config');
-
-            $migrationFileName = 'create_walkway_columns.php';
-            if (! $this->migrationFileExists($migrationFileName)) {
-                $path =  database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName);
-
-                $this->publishes([
-                    __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => $path,
-                ], 'walkway-migrations');
-            }
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         }
-    }
-
-    /**
-     * Check if the specified migration file already exists.
-     *
-     * @param  string  $migrationFileName
-     * @return bool
-     */
-    public static function migrationFileExists(string $migrationFileName): bool
-    {
-        $len = strlen($migrationFileName);
-        foreach (glob(database_path("migrations/*.php")) as $filename) {
-            if ((substr($filename, -$len) === $migrationFileName)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     /**
